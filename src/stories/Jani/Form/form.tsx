7a7a152a1e6ccee62c251d5cd34fd22e456ebc9e
Jani <martonjanos1990@gmail.com>
2023-07-21 21:23:28 +0200

    #

--- src/stories/Jani/Form/form.tsx
+++ src/stories/Jani/Form/form.tsx
@@ -1,36 +1,19 @@
 // @ts-ignore
 import React from 'react';
 import './form.scss';
-import {FormTypes,FormDataType} from "./interface";
+import { FormTypes, FormDataType } from "./interface";
 
-export const Form = ({...props}:FormTypes) => {
-    const [formDataInner, setFormDataInner] = React.useState<FormDataType>(props.formData);
-
-    const handleChange = (field) => {
-        const data = {
-            ...formDataInner,
-            [field.name]: field.value
-        }
-        setFormDataInner(data);
-        if (props.onChange) {
-            props.onChange(data);
-        }
-    }
+export const Form = ({ ...props }: FormTypes) => {
 
     return (
         <div className={'form'}>
-            {props.children &&
-                React.Children.map(props.children, (child) => {
-                    let modifiedChild = child;
-                    if (child.type.name === 'FormLayout') {
-                         modifiedChild = React.cloneElement(child, {
-                            onChange: handleChange,
-                            formData: formDataInner
-                         });
-                    }
-                    return modifiedChild;
-                })}
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
-
